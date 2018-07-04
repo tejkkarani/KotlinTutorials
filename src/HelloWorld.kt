3c2416f3192e4daa83d59888d165738fe46016e7
tejkkarani <tejkkarani@gmail.com>
2018-07-04 11:18:38 +0530

    added comments and a print statement

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,3 +1,4 @@
-fun main(args : Array<String>) {
-    println("Hello World")
+fun main(args : Array<String>) { //main function similar to java main function
+    println("Hello World")  //to print in new line println is used
+    print("Hello World") //else just print is used
 }
